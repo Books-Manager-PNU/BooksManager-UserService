624dcfb61251aec942199a64f2a2131c1f319113
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-04-24 17:31:31 +0300

    init project

---
+++ src/test/java/com/example/booksmanager/BooksManagerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksManagerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
